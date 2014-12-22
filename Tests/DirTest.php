d34aec37342f04c1e5d1125995fb88a5194108c4
Mickaël Andrieu <andrieu.travail@gmail.com>
2014-12-22 16:30:23 +0100

    Fixed copy tests from Dir class

--- Tests/DirTest.php
+++ Tests/DirTest.php
@@ -6,44 +6,33 @@ use BackBee\Utils\File\Dir;
 
 class DirTest extends \PHPUnit_Framework_TestCase
 {
-    private $copy_path;
+    private $copyFolder;
     private $privateFolder;
 
     public function setUp()
     {
-        $this->copyPath = 'file.txt';
+        $this->copyFolder = $this->getFixturesFolder().'archive';
         $this->privatePath = $this->getFixturesFolder().'bad-rights';
         @chmod($this->privatePath, 0000);
     }
 
     public function testCopy()
     {
-        /*$dir_mode = 0777;
-        $vfs_dir = vfsStream::setup('startpath', $dir_mode, array('startfile' => 'start data'));
-        $start_path = vfsStream::url('startpath');
+        $copyPath = $this->getFixturesFolder() . 'archive2';
+        $directoryPath = Dir::copy($this->copyFolder, $copyPath);
 
-        $copy_path = $this->copy_path;
+        $this->assertEquals(true, $directoryPath);
+        $this->assertEquals(array_values(Dir::getContent($copyPath)), array_values(Dir::getContent($this->copyFolder)));
 
-        $dir_path = Dir::copy($start_path, $copy_path);
-
-        $this->assertEquals(true, $dir_path);
-
-        $this->assertEquals(array_values(Dir::getContent($copy_path)), array_values(Dir::getContent($start_path)));
-        */
+        Dir::delete($copyPath);
     }
 
-    public function testUnreadbleCopy()
+    /**
+     * @expectedException PHPUnit_Framework_Error_Warning
+     */
+    public function testUnreadableCopy()
     {
-        /*$dir_mode = 0000;
-        $vfs_dir = vfsStream::setup('dircopy', $dir_mode, array('copyfile' => 'copy data'));
-
-        $start_path = vfsStream::url('dircopy');
-
-        $unreadable = vfsStream::setup('copydir', 0000);
-        $dir_path1 = Dir::copy($start_path, $unreadable);
-
-        $this->assertEquals(false, $dir_path1);
-        */
+        $this->assertFalse(Dir::copy($this->copyFolder, $this->privateFolder));
     }
 
     /**
@@ -152,15 +141,5 @@ class DirTest extends \PHPUnit_Framework_TestCase
     {
         @chmod($this->privatePath, 0755);
         $this->privatePath = null;
-        if (is_dir($this->copy_path)) {
-            $allfiles = scandir($this->copy_path);
-            foreach ($allfiles as $file) {
-                if (!is_dir($file)) {
-                    unlink($this->copy_path.DIRECTORY_SEPARATOR.$file);
-                }
-            }
-
-            rmdir($this->copy_path);
-        }
     }
 }
